9defde261f0989b3e2076f57be78238497747162
katiegirlsarah <69067202+katiegirlsarah@users.noreply.github.com>
2021-07-09 12:01:55 +1000

    Update trance.js

--- js/trance.js
+++ js/trance.js
@@ -1,12 +1,17 @@
 let eyeCursor = document.querySelector("#eyeCursor");
 
 let avgPoints = [];
+let first = true;
  
 webgazer.showVideo(false);
 webgazer.showPredictionPoints(false)
 webgazer.setRegression("weightedRidge"); //Prioritizes new user interactions!
 webgazer.applyKalmanFilter(true); // Apply a kalman filter to better track eyes
 webgazer.setGazeListener((data, elapsedTime) => {//made this into an arrow function
+    if(first){
+      first = false;
+      alertBox("Model Loaded! Click To Start");
+    }
     if (data == null) { 
         return;
     }
@@ -15,17 +20,17 @@ webgazer.setGazeListener((data, elapsedTime) => {//made this into an arrow funct
       
     let sumX = 0;
     for(let i=0;i<avgPoints.length;i++){
-        sumX+=avgPoints[i][0];
+      sumX+=avgPoints[i][0];
     }
     let sumY = 0;
     for(let j=0;j<avgPoints.length;j++){
-        sumY+=avgPoints[j][1];
+      sumY+=avgPoints[j][1];
     }
     let xPred = (sumX / avgPoints.length);
     let yPred = (sumY / avgPoints.length);
     eyeCursor.style.left = xPred+"px";
     eyeCursor.style.top = yPred+"px";
-    console.log(xPred,yPred);
+//     console.log(xPred,yPred);
 }).begin();
 
 //TODO: calibration, use webgazer.pause and webgazer.resume to stop and start user interaction data collection
